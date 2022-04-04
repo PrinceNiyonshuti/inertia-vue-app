44e4b7a31448a1ebe0fe1ab8cfae17a69e224725
PrinceNiyonshuti <npprince47@gmail.com>
2022-04-04 22:29:08 +0200

    passing props and file structure

---
+++ resources/js/Pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello ,{{ name }}</h1>
+    <p>In this series we will use :</p>
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+</template>
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks:Array
+    },
+};
+</script>
